e80ed4105e1e1bcc0564fc16a34f49dfff355765
BuildTools <unconfigured@null.spigotmc.org>
2022-12-13 12:16:38 -0500

    Added unit test

--- src/main/java/com/jenkintest/App.java
+++ src/main/java/com/jenkintest/App.java
@@ -10,6 +10,10 @@ public class App {
     public static void main(String[] args) {
         System.out.print("Type something: ");
         Scanner input = new Scanner(System.in);
-        System.out.println("You typed: " + input.nextLine());
+        System.out.println(checkInput(input.nextLine()));
+    }
+
+    public static String checkInput(String input) {
+        return "You typed: " + input;
     }
 }
--- src/test/java/com/jenkintest/AppTest.java
+++ src/test/java/com/jenkintest/AppTest.java
@@ -1,5 +1,6 @@
 package com.jenkintest;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -7,14 +8,11 @@ import org.junit.Test;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testPrint() {
+        App app = new App();
+        assertEquals("You typed: Hell", app.checkInput("Hello"));
     }
 }
